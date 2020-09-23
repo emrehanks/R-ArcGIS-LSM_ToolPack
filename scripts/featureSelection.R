a5b8d8e7027da364efb1f0a4eca6d9f68a0aa596
Emrehan Kutlug Sahin <54983600+emrehanks@users.noreply.github.com>
2020-09-23 10:55:40 +0300

    Update featureSelection.R

--- scripts/featureSelection.R
+++ scripts/featureSelection.R
@@ -1,6 +1,6 @@
 #####################################################################################################  
-### Article Name: A Novel Feature Selection Tool Based on Integrating R with ArcMap For Producing Landslide Susceptibility Mapping
-### Author(s): Emrehan Kutlug SAHýN ----- emrehansahin@ibu.edu.tr
+### Article Name: Developing Comprehensive Geocomputation Tools for Landslide Susceptibility Mapping: LSM Tool Pack
+### Author(s): Emrehan Kutlug SAHÃ½N ----- emrehansahin@ibu.edu.tr
 ###            Ismail COLKESEN -----  icolkesen@gtu.edu.tr
 ###            Aykut AKGUN  ----- aykutakgun@ktu.edu.tr
 ###            Arif Cagdas AYDINOGLU ----- aaydinoglu@gtu.edu.tr
@@ -277,7 +277,7 @@ tool_exec <- function(in_params, out_params)
       arc.progress_pos(as.integer(i * (100/featureFoldNumber)))
       listofweights[[i]] <- information.gain(train ~. , listoftrain[[i]])
     }
-  }else if(algoritm == "Spearman’s Rank Correlation Coefficient"){
+  }else if(algoritm == "SpearmanÂ’s Rank Correlation Coefficient"){
     for(i in 1:featureFoldNumber){
       arc.progress_pos(as.integer(i * (100/featureFoldNumber)))
       listofweights[[i]] <- rank.correlation(train ~. , listoftrain[[i]])
